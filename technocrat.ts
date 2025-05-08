45a6e425df1737e163bfe051d4bbf83b66bf8bea
adibahbab4108 <adibalvi476@gmail.com>
2025-05-08 11:16:23 +0600

    problem 3,4,5 solved

--- technocrat.ts
+++ technocrat.ts
@@ -2,9 +2,38 @@ function formatString(input: string, toUpper: boolean = true): string {
   return toUpper ? input.toLocaleUpperCase() : input.toLocaleLowerCase();
 }
 
-function filterByRating(
-  items: { title: string; rating: number }[]
-): { title: string; rating: number }[] {
+interface Item {
+  title: string;
+  rating: number;
+}
+function filterByRating(items: Item[]): Item[] {
   return items.filter((item) => item.rating >= 4.0);
 }
 
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  let newArray: T[] = [];
+  arrays.forEach((array) => newArray.push(...array));
+  return newArray;
+}
+
+class Vehicle {
+  private make: string;
+  private year: number;
+  constructor(brandName: string, year: number) {
+    this.make = brandName;
+    this.year = year;
+  }
+  getInfo() {
+    return `Make ${this.make}, Year ${this.year}`;
+  }
+}
+class Car extends Vehicle {
+  private model: string;
+  constructor(brandName: string, year: number, model: string) {
+    super(brandName, year);
+    this.model = model;
+  }
+  getModel() {
+    return `Model: ${this.model}`;
+  }
+}
